88f2af6c3a17952e131c13d0a32d4c828780a916
kietn20 <kietnguyen3698@gmail.com>
2024-05-23 13:37:45 -0700

    first commit

---
+++ gemini.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import { GoogleGenerativeAI } from "@google/generative-ai"
+
+dotenv.config()
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+async function run() {
+    const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro-latest" });
+    const prompt = "I am picking a Jungler for my draft. The enemy team consists of Rumble, Kindred, Lissandra, Jhin, and Gragas. What should I pick to counter the enemy's team kits?"
+    const result = await model.generateContent(prompt)
+    const response = await result.response;
+    const text = response.text();
+    console.log(`Response: ${JSON.stringify(response)}\n`);
+    console.log(`Text: ${text}`)
+}
+
+run();
