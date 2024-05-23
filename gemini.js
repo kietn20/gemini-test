d2dec3484ba68bfbf23d1d3ef6651657ff25b0ab
kietn20 <kietnguyen3698@gmail.com>
2024-05-23 13:42:05 -0700

    first commit

--- gemini.js
+++ gemini.js
@@ -6,11 +6,10 @@ const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 async function run() {
     const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro-latest" });
-    const prompt = "I am picking a Jungler for my draft. The enemy team consists of Rumble, Kindred, Lissandra, Jhin, and Gragas. What should I pick to counter the enemy's team kits?"
+    const prompt = "I am picking a Jungler for my draft. The enemy team consists of Rumble, Kindred, Lissandra, Jhin, and Gragas. What should I pick to counter the enemy's team kits? Please suggest 3 defensive and 3 offensive champions."
     const result = await model.generateContent(prompt)
     const response = await result.response;
     const text = response.text();
-    console.log(`Response: ${JSON.stringify(response)}\n`);
     console.log(`Text: ${text}`)
 }
 
